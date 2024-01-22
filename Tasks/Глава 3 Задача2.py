9336898e4a3e304b2d9a9d0eca8040ee8a510d0f
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-22 16:02:47 +0300

    Update Глава 3 Задача2.py

--- Tasks/Глава 3 Задача2.py
+++ Tasks/Глава 3 Задача2.py
@@ -8,9 +8,11 @@ def povorot_matrix(matrix):
     
     return result
 
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(povorot_matrix(matrix))  # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-
-matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
-print(povorot_matrix(matrix))  # [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+def main():
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(povorot_matrix(matrix))  # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    
+    matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+    print(povorot_matrix(matrix))  # [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+if __name__ == "__main__":
+    main()
