4e768468bfb58ed9447ee03506bf6a15582b7579
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-22 15:45:09 +0300

    Update Глава 1 Задача1.py

--- Tasks/Глава 1 Задача1.py
+++ Tasks/Глава 1 Задача1.py
@@ -2,6 +2,8 @@ def Palindrome(num):
     str_num = str(num)
     reverse_str_num = str_num[::-1]
     return str_num == reverse_str_num
-
-num = int(input("Введите целое число: "))
-print(Palindrome(num))
+def main():
+    num = int(input("Введите целое число: "))
+    print(Palindrome(num))
+if __name__ == "__main__":
+    main()
