51c4d8f9281ad14ba465de7abb4ccf611083c21e
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-22 16:12:52 +0300

    Update Глава 7 Задача1.py

--- Tasks/Глава 7 Задача1.py
+++ Tasks/Глава 7 Задача1.py
@@ -9,6 +9,9 @@ def santa_users(users_list):
         users_dict[name] = index
     return users_dict
 
-users = [["name1 surname1", 12345], ["name2 surname2"], ["name3 surname3", 12354], ["name4 surname4", 12435]]
-result = santa_users(users)
-print(result)
+def main():
+    users = [["name1 surname1", 12345], ["name2 surname2"], ["name3 surname3", 12354], ["name4 surname4", 12435]]
+    result = santa_users(users)
+    print(result)
+if __name__ == "__main__":
+    main()
