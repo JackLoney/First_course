cd131b6ee89fce84895e76f6303875db069433de
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-25 22:53:16 +0300

    Update Глава 7 Задача2.py

--- Tasks/Глава 7 Задача2.py
+++ Tasks/Глава 7 Задача2.py
@@ -1,12 +1,12 @@
 def roman_to_arabic(s):
-    Slovarik = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    Roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
     for i in range(len(s)):
-        if i > 0 and Slovarik[s[i]] > Slovarik[s[i - 1]]:
-            result += Slovarik[s[i]] - 2 * Slovarik[s[i - 1]]
+        if i > 0 and Roman[s[i]] > Roman[s[i - 1]]:
+            result += Roman[s[i]] - 2 * Roman[s[i - 1]]
         else:
-            result += Slovarik[s[i]]
+            result += Roman[s[i]]
 
     return result
 
