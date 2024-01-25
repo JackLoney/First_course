edf052108ca42d4f3f7664e106bed9b44fa95028
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-25 22:42:17 +0300

    Update Глава 3 Задача2.py

--- Tasks/Глава 3 Задача2.py
+++ Tasks/Глава 3 Задача2.py
@@ -1,4 +1,4 @@
-def povorot_matrix(matrix):
+def Turn_matrix(matrix):
     n = len(matrix[0])
     result = [[0] * n for _ in range(n)]
     
@@ -10,9 +10,9 @@ def povorot_matrix(matrix):
 
 def main():
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    print(povorot_matrix(matrix))  # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    print(Turn_matrix(matrix))  # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
     
     matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
-    print(povorot_matrix(matrix))  # [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+    print(Turn_matrix(matrix))  # [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
 if __name__ == "__main__":
     main()
