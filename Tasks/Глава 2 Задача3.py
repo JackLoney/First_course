31a66395d4ec63d24123c9d647563ae4624fa3e4
JackLoney <157331050+JackLoney@users.noreply.github.com>
2024-01-31 12:49:10 +0300

    Update Глава 2 Задача3.py

--- Tasks/Глава 2 Задача3.py
+++ Tasks/Глава 2 Задача3.py
@@ -1,49 +1,30 @@
 def check_brackets(s):
+    brackets = {'(': ')', '{': '}', '[': ']'}
     stack = []
-    mapping = {')': '(', '}': '{', ']': '['}
-
     for char in s:
-        if char in '({[':
+        if char in brackets.keys():
             stack.append(char)
-        elif char in ')}]':
-            if not stack or stack[-1] != mapping[char]:
+        elif char in brackets.values():
+            if not stack or brackets[stack.pop()] != char:
                 return False
-            stack.pop()
-
+    
     return len(stack) == 0
 
-def longest_valid_substring(s):
-    stack = []
-    start = 0
-    max_length = 0
-    longest_substring = None
-
-    for i, char in enumerate(s):
-        if char in '({[':
-            stack.append(i)
-        elif stack:
-            stack.pop()
-            if stack:
-                length = i - stack[-1]
-            else:
-                length = i - start + 1
-
-            if length > max_length:
-                max_length = length
-                longest_substring = s[start:i+1]
-        else:
-            start = i + 1
-
-    return longest_substring if longest_substring else False
-def main():
-    s = input("Введите строку скобок: ")
+def longest_brackets(s):
+    remember = int()
+    remember_max = ''
     if check_brackets(s):
-            print("True")
+        return True
     else:
-        longest_substring = longest_valid_substring(s)
-        if longest_substring:
-            print(longest_substring)
-        else:
-            print("False")
+        for i in range(1, len(s)-1):
+            for j in range (i+1, len(s)):
+                if check_brackets(s[i:j]):
+                    if remember < len(s[i:j]):
+                        remember = len(s[i:j])
+                        remember_max = s[i:j]
+    return remember_max
+def main():
+    a = input('Введите строку скобок: ')
+    print(longest_brackets(a))
 if __name__ == "__main__":
     main()
